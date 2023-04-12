1c2982d3c7d49e6bd0b2fef63a52af2e8844b71e
Phong Vo <phong.d@nimblehq.co>
2023-04-12 08:49:10 +0700

    [#1] Change min Android target and update code owner

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 33
     defaultConfig {
         applicationId = "co.nimblehq.avishek.phong.kmmic.android"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
