a78baacc9bab49c02a14a7f3e167014370599e72
Phong Vo <phong.d@nimblehq.co>
2023-04-24 08:56:23 +0700

    [#6] Update the kotlinCompilerExtensionVersion

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.0"
+        kotlinCompilerExtensionVersion = "1.3.1"
     }
     packagingOptions {
         resources {
