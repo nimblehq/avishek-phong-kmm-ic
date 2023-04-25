e86cf660a82c0dc514e6cf53b25b15087e39c391
Avishek Khan <avishek@nimblehq.co>
2023-04-25 12:09:12 +0700

    [#81] [CHORE] Set up CI/CD workflow for distributing Android staging app to Firebase

--- buildSrc/src/main/kotlin/appPackage/Configurations.kt
+++ buildSrc/src/main/kotlin/appPackage/Configurations.kt
@@ -1,6 +1,12 @@
 object Flavors {
     const val PRODUCTION = "production"
     const val STAGING = "staging"
+    const val DIMENSION_VERSION = "version"
+}
+
+object BuildTypes {
+    const val DEBUG = "debug"
+    const val RELEASE = "release"
 }
 
 object XcodeConfiguration {
