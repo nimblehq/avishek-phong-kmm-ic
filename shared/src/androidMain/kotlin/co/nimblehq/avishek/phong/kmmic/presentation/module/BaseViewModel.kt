8446596b2a387a1dfe5f906b8bcfe2d6bcc9d7d2
Avishek Khan <avishek@nimblehq.co>
2023-05-05 13:01:04 +0700

    [$22] [Android] [Integrate] Implement login

--- shared/src/androidMain/kotlin/co/nimblehq/avishek/phong/kmmic/presentation/module/BaseViewModel.kt
+++ shared/src/androidMain/kotlin/co/nimblehq/avishek/phong/kmmic/presentation/module/BaseViewModel.kt
@@ -1,12 +1,13 @@
 package co.nimblehq.avishek.phong.kmmic.presentation.module
 
+import androidx.lifecycle.viewModelScope as lifecycleViewModelScope
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 
 actual abstract class BaseViewModel : ViewModel() {
 
-    actual val viewModelScope = MainScope()
+    actual val viewModelScope: CoroutineScope = lifecycleViewModelScope
 
     fun clear() {
         viewModelScope.cancel()
