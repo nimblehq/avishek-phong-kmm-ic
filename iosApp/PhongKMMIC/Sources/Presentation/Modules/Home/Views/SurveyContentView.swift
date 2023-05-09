c06b3245085d97fb21cc3a7c55c997bfc0242b2f
Phong Vo <phong.d@nimblehq.co>
2023-05-09 15:34:15 +0700

    [#36] Remove unused code

--- iosApp/PhongKMMIC/Sources/Presentation/Modules/Home/Views/SurveyContentView.swift
+++ iosApp/PhongKMMIC/Sources/Presentation/Modules/Home/Views/SurveyContentView.swift
@@ -12,7 +12,6 @@ struct SurveyContentView: View {
 
     var uiModels: [UIModel]
     @State private var currentPage = 0
-    @State private var nextPage = 0
     @State private var currentVisibility: Double = 1.0
 
     @DragGestureVelocity private var velocity: CGVector
@@ -22,34 +21,13 @@ struct SurveyContentView: View {
     private var draggingVisibilityMultiplier: Double = 1.2
 
     var body: some View {
-        // swiftlint:disable:next closure_body_length
         GeometryReader { geometryReader in
             VStack {
                 PageViewIndicator(numberOfPage: uiModels.count, currentPage: $currentPage)
                     .frame(width: geometryReader.size.width - 40.0, alignment: .leading)
                     .padding([.bottom], 20.0)
-                Text(uiModels[safe: currentPage]?.title ?? "")
-                    .foregroundColor(.white)
-                    .font(.boldTitle)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .multilineTextAlignment(.leading)
-                    .lineLimit(2)
-                HStack {
-                    Text(uiModels[safe: currentPage]?.description ?? "")
-                        .foregroundColor(.white.opacity(0.7))
-                        .font(.regularBody)
-                        .lineLimit(2)
-                    Spacer()
-                    Button {
-                        // TODO: Handle button taps
-                        print("Next button was tapped")
-                    } label: {
-                        R.image.rightChevron.image
-                            .frame(width: 56.0, height: 56.0)
-                            .background(Color.white)
-                            .clipShape(Circle())
-                    }
-                }
+                titleView
+                descriptionAndNextButtonView
             }
             .padding(.horizontal, 20.0)
             .frame(
@@ -73,14 +51,6 @@ struct SurveyContentView: View {
         .gesture(
             DragGesture(minimumDistance: 5.0, coordinateSpace: .local)
                 .onChanged { value in
-                    switch value.translation.width {
-                    case ...0: // Right to left
-                        nextPage = min(uiModels.count - 1, currentPage + 1)
-                    case 0...:
-                        nextPage = max(0, currentPage - 1)
-                    default:
-                        return
-                    }
                     let turningVisibilityPercentage =
                         draggingLength * draggingVisibilityMultiplier / abs(value.translation.width)
                     currentVisibility = max(0.0, turningVisibilityPercentage)
@@ -107,6 +77,34 @@ struct SurveyContentView: View {
                 }
                 .updatingVelocity($velocity)
         )
+    }
+
+    var titleView: some View {
+        Text(uiModels[safe: currentPage]?.title ?? "")
+            .foregroundColor(.white)
+            .font(.boldTitle)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .multilineTextAlignment(.leading)
+            .lineLimit(2)
+    }
+
+    var descriptionAndNextButtonView: some View {
+        HStack {
+            Text(uiModels[safe: currentPage]?.description ?? "")
+                .foregroundColor(.white.opacity(0.7))
+                .font(.regularBody)
+                .lineLimit(2)
+            Spacer()
+            Button {
+                // TODO: Handle button taps
+                print("Next button was tapped")
+            } label: {
+                R.image.rightChevron.image
+                    .frame(width: 56.0, height: 56.0)
+                    .background(Color.white)
+                    .clipShape(Circle())
+            }
+        }
     }
 
     init(uiModels: [UIModel]) {
@@ -140,13 +138,13 @@ struct SurveyContentView_Previews: PreviewProvider {
             id: 0,
             title: "Career training and development",
             description: "We would like to know what are your goals ans skill you wanted to develop",
-            imageUrl: "https://image-1.uhdpaper.com/b/phone-4k/apple-2021-logo-4k-phone-wallpaper-2160x3840-uhdpaper.com-945.1_b.jpg"
+            imageUrl: "https://dhdbhh0jsld0o.cloudfront.net/m/1ea51560991bcb7d00d0_"
         ),
         SurveyContentView.UIModel(
             id: 0,
             title: "Career development and training",
             description: "We would like to know what are your goals ans skill you wanted to develop",
-            imageUrl: "https://image-0.uhdpaper.com/wallpaper/apple-logo-black-background-phone-wallpaper-4k-uhdpaper.com-668@0@e.jpg"
+            imageUrl: "https://dhdbhh0jsld0o.cloudfront.net/m/6ea42840403875928db3_"
         )
     ]
 
--- iosApp/PhongKMMIC/Sources/Supports/Extensions/Foundation/Collection+SafeIndex.swift
+++ iosApp/PhongKMMIC/Sources/Supports/Extensions/Foundation/Collection+SafeIndex.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 extension Collection {
+
     subscript (safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
+        indices.contains(index) ? self[index] : nil
     }
 }
