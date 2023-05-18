e3e522a7babd571f316251ff64b68e780dc5ddb3
Avishek Khan <avishek@nimblehq.co>
2023-05-18 17:48:23 +0700

    [#53] [Android] [UI] As a user, I can answer a question with multiple forms

---
+++ androidApp/src/main/java/co/nimblehq/avishek/phong/kmmic/android/ui/screen/surveydetail/TextFields.kt
@@ -0,0 +1,46 @@
+package co.nimblehq.avishek.phong.kmmic.android.ui.screen.surveydetail
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import co.nimblehq.avishek.phong.kmmic.android.ui.common.PrimaryTextField
+import co.nimblehq.avishek.phong.kmmic.presentation.uimodel.SurveyAnswerUiModel
+
+@Composable
+fun TextFields(
+    surveyAnswerUiModels: List<SurveyAnswerUiModel>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier
+    ) {
+        items(surveyAnswerUiModels.size) { index ->
+            var value by remember { mutableStateOf("") }
+
+            PrimaryTextField(
+                value = value,
+                onValueChange = {
+                    value = it
+                },
+                placeholder = surveyAnswerUiModels[index].placeholder.orEmpty(),
+                imeAction = if (index == surveyAnswerUiModels.lastIndex) ImeAction.Done else ImeAction.Next
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TextFieldPreview(
+    @PreviewParameter(SurveyDetailScreenPreviewParameterProvider::class) params: SurveyDetailScreenPreviewParameterProvider.Params
+) {
+    TextFields(
+        surveyAnswerUiModels = params.survey.questionUiModels[0].answers
+    )
+}
